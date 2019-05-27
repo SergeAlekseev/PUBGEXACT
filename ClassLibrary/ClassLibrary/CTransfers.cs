5a1b2753f277fdc8de067ce72c0456624282a5db
Василий <MichVich99fm@gmail.com>
2019-05-27 21:35:04 +0300

    Добавлены тесты к сетевой части (CTransfer)

--- ClassLibrary/ClassLibrary/CTransfers.cs
+++ ClassLibrary/ClassLibrary/CTransfers.cs
@@ -1,10 +1,8 @@
 ﻿using Newtonsoft.Json;
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Net.Sockets;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace ClassLibrary
 {
