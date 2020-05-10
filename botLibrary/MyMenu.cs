dd3cc71a26bf107666517f9a173ec36b8587d0c8
SergeAlekseev <serge.alekseev999@gmail.com>
2020-05-10 21:40:37 +0300

    Added botLybrary

---
+++ botLibrary/MyMenu.cs
@@ -0,0 +1,77 @@
+﻿using ClassLibrary;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace botLibrary
+{
+	public partial class MyMenu : Form
+	{
+		ModelClient model = new ModelClient();
+		CMyMenu Controller;
+
+		public MyMenu(string Name, string Pass, string ip)
+		{
+			InitializeComponent();
+			Controller = new CMyMenu(model);
+			Controller.JoinUser(Name, Pass);
+			InfoIP.Text = ip;
+		}
+
+		public void button1_Click(object sender, EventArgs e)
+		{
+			try
+			{
+				if (Controller.Connect(InfoIP.Text) && !Controller.flagGame)
+				{
+					TcpClient client = new TcpClient(InfoIP.Text, 3337);
+					NetworkStream nStream = client.GetStream();
+					Client form = new Client(model.GInfo.Name, model.GInfo.Password, InfoIP.Text);
+					form.Show();
+					this.Hide();
+				}
+				else
+				{
+					MessageBox.Show("Подождите, игра идёт");
+				}
+			}
+			catch
+			{
+				MessageBox.Show("IP недоступен");
+			}
+		}
+
+		private void MyMenu_FormClosed(object sender, FormClosedEventArgs e)
+		{
+			Application.Exit();
+		}
+
+		private void MyMenu_Load(object sender, EventArgs e)
+		{
+			if (Controller.Connect(InfoIP.Text))
+			{
+				InfoName.Text = model.GInfo.Name;
+				InfoKills.Text = "" + model.GInfo.Kills;
+				InfoWins.Text = "" + model.GInfo.Wins;
+				dies.Text = "" + model.GInfo.Dies;
+				if (Controller.flagGame)
+				{
+					MessageBox.Show("Подождите, игра идёт");
+				}
+			}
+			else
+			{
+				InfoName.Text = "Неверный пароль";
+				bPlay.Enabled = false;
+			}
+
+		}
+	}
+}
---
+++ botLibrary/View.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace botLibrary
+{
+	partial class Client
+	{
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		/// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Код, автоматически созданный конструктором форм Windows
+
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Client));
+			this.PlayingField = new System.Windows.Forms.PictureBox();
+			this.timerPaint = new System.Windows.Forms.Timer(this.components);
+			this.timerMouseLocation = new System.Windows.Forms.Timer(this.components);
+			this.InfoName = new System.Windows.Forms.Label();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			((System.ComponentModel.ISupportInitialize)(this.PlayingField)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// PlayingField
+			// 
+			this.PlayingField.BackColor = System.Drawing.SystemColors.ActiveCaption;
+			this.PlayingField.Location = new System.Drawing.Point(12, 27);
+			this.PlayingField.Name = "PlayingField";
+			this.PlayingField.Size = new System.Drawing.Size(600, 600);
+			this.PlayingField.TabIndex = 0;
+			this.PlayingField.TabStop = false;
+			this.PlayingField.Click += new System.EventHandler(this.PlayingField_Click);
+			this.PlayingField.MouseDown += new System.Windows.Forms.MouseEventHandler(this.PlayingField_MouseDown);
+			this.PlayingField.MouseUp += new System.Windows.Forms.MouseEventHandler(this.PlayingField_MouseUp);
+			// 
+			// timerPaint
+			// 
+			this.timerPaint.Interval = 10;
+			this.timerPaint.Tick += new System.EventHandler(this.timerPaint_Tick);
+			// 
+			// timerMouseLocation
+			// 
+			this.timerMouseLocation.Tick += new System.EventHandler(this.timerMouseLocation_Tick);
+			// 
+			// InfoName
+			// 
+			this.InfoName.AutoSize = true;
+			this.InfoName.Location = new System.Drawing.Point(12, 9);
+			this.InfoName.Name = "InfoName";
+			this.InfoName.Size = new System.Drawing.Size(35, 13);
+			this.InfoName.TabIndex = 2;
+			this.InfoName.Text = "label1";
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(252, 8);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(35, 13);
+			this.label1.TabIndex = 3;
+			this.label1.Text = "label1";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(463, 8);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(35, 13);
+			this.label2.TabIndex = 4;
+			this.label2.Text = "label2";
+			// 
+			// Client
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(624, 639);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.InfoName);
+			this.Controls.Add(this.PlayingField);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Name = "Client";
+			this.Text = "Client";
+			this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Client_FormClosing);
+			this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Client_FormClosed);
+			this.Paint += new System.Windows.Forms.PaintEventHandler(this.Client_Paint);
+			this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+			this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Client_KeyPress);
+			this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+			((System.ComponentModel.ISupportInitialize)(this.PlayingField)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.PictureBox PlayingField;
+		private System.Windows.Forms.Timer timerPaint;
+		private System.Windows.Forms.Timer timerMouseLocation;
+		private System.Windows.Forms.Label InfoName;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+	}
+}
+
