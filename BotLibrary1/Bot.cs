59f216054f6790db3340edcd51b214ad513630ba
VasilyMikhailov <vasily.mikhailov@exactprosystems.com>
2020-05-10 22:18:07 +0300

    Create library for bot jobing

---
+++ BotLibrary1/Bot.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BotLibrary1
+{
+	public class Bot
+	{
+		public bool join()
+		{
+			//1. Авторизация бота
+
+
+			
+			//2. Подключение бота на сервер
+
+			return true;
+		}
+	}
+}
