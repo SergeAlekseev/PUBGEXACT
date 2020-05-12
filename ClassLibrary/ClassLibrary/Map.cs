489cf7a1788dda4196c2c48806d2ac5388bbfb2d
VasilyMikhailov <vasily.mikhailov@exactprosystems.com>
2020-05-13 02:11:38 +0300

    Create base method for moving bot

--- ClassLibrary/ClassLibrary/Map.cs
+++ ClassLibrary/ClassLibrary/Map.cs
@@ -14,7 +14,7 @@ namespace ClassLibrary
 		 List<Bush> listBush = new List<Bush>();
 		 List<Tree> listTrees = new List<Tree>();
 
-		public Rectangle mapBorders = new Rectangle(0, 0, 6000, 6000); 
+		public Rectangle mapBorders = new Rectangle(0, 0, 500, 500); 
 
 		List<Item> items = new List<Item>();
 		Zone nextZone = new Zone();
